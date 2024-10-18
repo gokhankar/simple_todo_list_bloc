92c75d06642cb15309ae73a106e52add909eba4a
Gökhan Karaçay <63194790+gokhankar@users.noreply.github.com>
2024-10-18 15:42:58 +0200

    organized files

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:todo_list/add_todo_page.dart';
+import 'package:todo_list/pages/add_todo_page.dart';
 import 'package:todo_list/cubit/todo_cubit.dart';
-import 'package:todo_list/todo_list.dart';
+import 'package:todo_list/components/todo_list.dart';
 
 void main() {
   runApp(const MyApp());
